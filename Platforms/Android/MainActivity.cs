e25a3bf07ec12bfd4575c5246b3ff46fdf186965
javierMo <morrins@gmail.com>
2025-09-07 10:08:23 +0200

    Funciona con teclado + corrección de la actualización del objetivo parcial del juego

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace AritmetrisMAUI
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        public override bool DispatchKeyEvent(KeyEvent e)
+        {
+            // Redirigimos todas las teclas a la página de juego si está activa.
+            // Si la página las consume, no seguimos con el pipeline normal.
+            if (Aritmetris.Pages.GamePage.HandleAndroidKeyEventStatic?.Invoke(e) == true)
+                return true;
+
+            return base.DispatchKeyEvent(e);
+        }
+    }
+}
